164fb76ad517659302742278987a49e1635eba56
pushpendra-mehta-github <mehtapushpendra6376@gmail.com>
2024-11-28 11:33:42 +0530

    install Figlet & Package

---
+++ Backend/Node JS/FigletDir/index.js
@@ -0,0 +1,10 @@
+let figlet = require("figlet");
+
+figlet("Pushpendra Mehta !!", function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  });
