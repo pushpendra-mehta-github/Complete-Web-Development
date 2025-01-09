7a44dbe54761597e3910bfe624919e3e757a15db
pushpendra-mehta-github <mehtapushpendra6376@gmail.com>
2025-01-09 15:54:53 +0530

    Handling POST Request

--- Backend/Miscellaneous/Back1/index.js
+++ Backend/Miscellaneous/Back1/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.get("/register", (req,res)=> {
    let { user,password } =  req.query;
@@ -9,8 +11,10 @@ app.get("/register", (req,res)=> {
    res.send(`Standared GET Request. Welcome ${user} ! `);
 });
 
+
 app.post("/register", (req,res)=> {
-    res.send("Standared POST Request ");
+  let { user,password } =  req.body;
+    res.send(`Standared POST Request. Welcome ${user} ! `);
   });
 
   
